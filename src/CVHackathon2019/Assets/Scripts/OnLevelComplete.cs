9d2b122199383b342ad7ea093c8f976021613e5b
Silas Reinagel <silas.reinagel@carvana.com>
2019-08-07 13:12:57 -0700

    Added Level Complete condition

---
+++ src/CVHackathon2019/Assets/Scripts/OnLevelComplete.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class OnLevelComplete : MonoBehaviour
+{
+    public GameObject ToActivate;
+    private bool _wasTriggered;
+    
+    void Update()
+    {
+        if (!_wasTriggered && GameState.Current.IsLevelComplete)
+        {
+            _wasTriggered = true;
+            ToActivate.SetActive(true);
+        }
+    }
+}
