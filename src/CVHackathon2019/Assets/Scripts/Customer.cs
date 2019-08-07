95f230d9d76a85d8f8f6382b24310abbe5d32aa9
Silas Reinagel <silas.reinagel@carvana.com>
2019-08-07 13:32:53 -0700

    Collisions. Customer's Served

--- src/CVHackathon2019/Assets/Scripts/Customer.cs
+++ src/CVHackathon2019/Assets/Scripts/Customer.cs
@@ -8,4 +8,23 @@ public class Customer : MonoBehaviour
     {
         GetComponent<Rigidbody2D>().AddForce(new Vector2(Speed, 0));
     }
+    
+    private void OnCollisionEnter(Collision other) => HandleCollision(other.gameObject);
+    private void OnCollisionEnter2D(Collision2D other) => HandleCollision(other.gameObject);
+    private void OnTriggerEnter(Collider other) => HandleCollision(other.gameObject);
+    private void OnTriggerEnter2D(Collider2D other) => HandleCollision(other.gameObject);
+
+    private void HandleCollision(GameObject other)
+    {
+        const int carLayer = 8;
+        if (other.layer == carLayer)
+            OnCarCollide(other);
+    }
+
+    private void OnCarCollide(GameObject other)
+    {
+        GameState.Current.OnCustomerServed();
+        Destroy(gameObject);
+        Destroy(other);
+    }
 }
--- src/CVHackathon2019/Assets/Scripts/GameState.cs
+++ src/CVHackathon2019/Assets/Scripts/GameState.cs
@@ -1,16 +1,19 @@
-﻿public sealed class GameState
+﻿using UnityEngine;
+
+public sealed class GameState
 {
     public bool IsGameOver => Outcome == LevelOutcome.GameOver;
     public bool IsLevelComplete => Outcome == LevelOutcome.Complete;
     public LevelOutcome Outcome { get; set; } = LevelOutcome.Incomplete;
-    public int NumCustomersServed { get; private set; }
-    public int NumCustomersRequired { get; private set; }
+    public int NumCustomersServed { get; private set; } = 0;
+    public int NumCustomersRequired { get; private set; } = 10;
     
     public void OnCustomerServed()
     {
         NumCustomersServed++;
         if (NumCustomersServed >= NumCustomersRequired)
             Outcome = LevelOutcome.Complete;
+        Debug.Log($"{NumCustomersServed} / {NumCustomersRequired}");
     }
     
     public static GameState Current = new GameState();
