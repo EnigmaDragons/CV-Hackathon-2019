7fccb91a410eddebb16b911b43948132bd2f3b50
Silas Reinagel <silas.reinagel@carvana.com>
2019-08-07 09:43:40 -0700

    Created Customer Spawner. Closes #8

---
+++ src/CVHackathon2019/Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float SpawnIntervalSeconds = 1.5f;
+    public bool IsGameOver = false;
+    public GameObject[] Customers;
+    
+    void Start()
+    {
+        StartCoroutine(Countdown());
+    }
+
+    private IEnumerator Countdown()
+    {
+        while(!IsGameOver)
+        {
+            yield return new WaitForSeconds(SpawnIntervalSeconds);
+            SpawnCustomer();
+        }
+    }
+
+    private void SpawnCustomer()
+    {
+        var customerPrototype = Customers[0];
+        Instantiate(customerPrototype, new Vector3(100, 400, 0), Quaternion.identity);
+    }
+}
