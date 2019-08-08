619ab4cd9e7c741ec0898d746d55e5b3ca302845
Silas Reinagel <silas.reinagel@carvana.com>
2019-08-08 16:23:01 -0700

    Score text. Closes #136

--- src/CVHackathon2019/Assets/Scripts/UI/Scoreboard.cs
+++ src/CVHackathon2019/Assets/Scripts/UI/Scoreboard.cs
@@ -1,25 +1,18 @@
+using System.Globalization;
 using UnityEngine;
 using TMPro;
 
 public class Scoreboard : MonoBehaviour
 {
-
 	public TextMeshProUGUI scoreText;
-	public int score;
-
-	private int _gameScore => GameState.Current.GameScore;
 	
 	void Start ()
 	{
 		scoreText = GetComponent<TextMeshProUGUI>();
-	    score = _gameScore;
 	}
 
 	void Update ()
 	{
-		score = _gameScore;
-	    GameState.Current.GameScore = score;
-	    scoreText.text = "$" + score;
+	    scoreText.text = string.Format(CultureInfo.InvariantCulture, "{0:N0}", GameState.Current.GameScore);
 	}
-
 }
