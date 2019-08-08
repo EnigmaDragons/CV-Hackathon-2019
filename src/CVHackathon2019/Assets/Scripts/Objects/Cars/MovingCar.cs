85f82587b6fd51d7019da90d48be00b6699fffea
Alec Ruderman <alec.ruderman@carvana.com>
2019-08-08 08:19:24 -0700

    Added HitBox to Hauler and Script To Car

---
+++ src/CVHackathon2019/Assets/Scripts/Objects/Cars/MovingCar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MovingCar : MonoBehaviour
+{
+    public bool IsReturn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
