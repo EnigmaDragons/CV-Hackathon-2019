4b084c8d56cd3a5141075fafe96e38994aa166d7
Anuj Agrawal <anuj.agrawal@carvana.com>
2019-08-08 14:13:37 -0700

    Scoring changing first pass wip

--- src/CVHackathon2019/Assets/Scripts/UI/Scoreboard.cs
+++ src/CVHackathon2019/Assets/Scripts/UI/Scoreboard.cs
@@ -7,6 +7,15 @@ public class Scoreboard : MonoBehaviour
 	public TextMeshProUGUI scoreText;
 	private int score;
 
+	// multipliers
+	public int Difficulty = 1;
+	public int Level = 1;
+
+	private int _stars => GameState.Current.StarRatings;
+
+
+	public int CarDeliveredCashValue = 1000;
+
 	void Start ()
 	{
 		scoreText = GetComponent<TextMeshProUGUI>();
@@ -19,9 +28,25 @@ public class Scoreboard : MonoBehaviour
 	    scoreText.text = "$" + score;
 	}
 
+	// in progress, global state vars, etc
+	int CalculateScore() {
+		var curScore = score;
+
+		var numServed = GameState.Current.NumCustomersServed;
+
+		var gameplayMultipliers = 1*Level*Difficulty;
+		var cashMultipliers = 1*CarDeliveredCashValue*_stars;
+		var multipliers = gameplayMultipliers*cashMultipliers;
+
+		var newScore = 1*multipliers*numServed;
+
+		var scoreCalc = newScore;
+		return scoreCalc;
+	}
+
 	void Update ()
 	{
-		score = 1000*GameState.Current.NumCustomersServed;
+		score = CalculateScore();
 	    UpdateScore ();
 	}
 
