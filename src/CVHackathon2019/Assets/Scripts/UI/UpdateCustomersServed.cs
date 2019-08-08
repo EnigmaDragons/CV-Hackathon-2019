c8991e9893f0c93e0f14c6dc062b9f1725a305d6
Silas Reinagel <silas.reinagel@carvana.com>
2019-08-08 16:08:40 -0700

    Customer Served UI Widget

---
+++ src/CVHackathon2019/Assets/Scripts/UI/UpdateCustomersServed.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UpdateCustomersServed : MonoBehaviour
+{
+    private TextMeshProUGUI _text;
+    
+    void Start()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        _text.text = GameState.Current.NumCustomersServed.ToString();
+    }
+}
