ce4fa4f2f1910f454a7f2c4f0d5da06467ce78f6
Silas Reinagel <silas.reinagel@carvana.com>
2019-08-08 13:31:31 -0700

    Wired up stars

---
+++ src/CVHackathon2019/Assets/Scripts/UI/MinStarsNeeded.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MinStarsNeeded : MonoBehaviour
+{
+    public int MinStars = 1;
+
+    private Image _image;
+    
+    void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        _image.enabled = GameState.Current.StarRatings >= MinStars;
+    }
+}
