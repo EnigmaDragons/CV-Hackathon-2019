b38f50a76de28f193aebe9a13341ef642ef65727
Robby Mitchell <robjmitch@gmail.com>
2019-08-09 10:59:16 -0700

    Adds tutorial

---
+++ src/CVHackathon2019/Assets/Scripts/Menu/Tutorial.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject Tuts;
+
+    public void OnClick()
+    {
+        Tuts.SetActive(true);
+    }
+
+    public void OnReturn()
+    {
+        Tuts.SetActive(false);
+    }
+}
